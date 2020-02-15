3d4803eaf9d25d70b49f1aa9bf01f7691e493648
Filip <fiqxio@gmail.com>
2020-02-15 13:20:54 +0100

    client request method adding userkey to requests

--- src/client.ts
+++ src/client.ts
@@ -14,12 +14,19 @@ export class Client {
 		this._config = config
 		if (!this._config.userkey) {
 			this.getUserKey()
-			this.request('entries/add', { postParams: { body: 'siema' } })
 		}
 	}
 	private async request<T>(endpoint: string, params: IRequestParams = {}, requestOptions?: IRequestOptions) {
+		if (!params.namedParams) {
+			params.namedParams = { }
+		}
+		if (!params.namedParams.userkey) {
+			if (!this._config.userkey) {console.warn('Trying to authorize request with empty userkey')}
+			params.namedParams.userkey = this._config.userkey
+		}
 		try {
-			const response = await this._ctx.request(endpoint, params, requestOptions)
+			const response = await this._ctx.request<T>(endpoint, params, requestOptions)
+			console.log(response)
 		} catch (error) {
 			console.log(error)
 		}
